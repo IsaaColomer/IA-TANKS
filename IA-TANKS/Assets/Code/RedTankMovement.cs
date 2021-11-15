d38b07c17fcc084a3238011e710dae4905364f47
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-11-15 11:21:34 +0100

    Red tank wander works, but can be improved

---
+++ IA-TANKS/Assets/Code/RedTankMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class RedTankMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Wander();
+    }
+    void Wander()
+    {
+        float radius = 2f;
+        float offset = 3f;
+        Vector3 localTarget = new Vector3(
+            Random.Range(-1.0f, 1.0f), 0,
+            Random.Range(-1.0f, 1.0f));
+        localTarget.Normalize();
+        localTarget *= radius;
+        localTarget += new Vector3(0, 0, offset);
+        Vector3 worldTarget =
+            transform.TransformPoint(localTarget);
+        worldTarget.y = 0f;
+
+        Vector3 result = Vector3.zero;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(worldTarget, out hit, 1.0f, NavMesh.GetAreaFromName("Not Walkable")))
+        {
+            Debug.Log("In");
+            agent.destination = worldTarget;
+        }
+        else
+        {
+            Debug.Log("Out");
+            agent.destination = -worldTarget;
+        }
+
+    }
+}
