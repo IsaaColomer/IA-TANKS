29d22abd921b4cae42006336173b26e09403a570
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-11-22 14:40:58 +0100

    Worksx2

--- IA-TANKS/Assets/Code/BlueBullet.cs
+++ IA-TANKS/Assets/Code/BlueBullet.cs
@@ -8,18 +8,28 @@ public class BlueBullet : MonoBehaviour
     [SerializeField] private float time;
     private Rigidbody rb;
     private float velocity;
-    private Transform redTank;
+    [SerializeField] private Transform blueTank;
+    [SerializeField] private Vector3 blueTank2;
+    [SerializeField] private Transform firePoint;
     private Vector2 updateVector;
     private float arrel;
     private Vector3 targetXZPos;
+    private float distance;
+    Vector3 localVelocity;
+    [SerializeField] private Vector3 startpos;
+    Vector3 globalVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        redTank = GameObject.FindGameObjectWithTag("Red").transform;
-
+        rb = gameObject.GetComponent<Rigidbody>();
+        blueTank = GameObject.FindGameObjectWithTag("RedFirePoint").transform;
+        blueTank2 = transform.TransformDirection(blueTank.transform.position);
+        firePoint = GameObject.FindGameObjectWithTag("BlueFirePoint").transform;
+        velocity = 10f;
         time = 0;
-        transform.LookAt(BlueTankBullet.instace.enemy.transform.position);
+        transform.LookAt(blueTank.transform.position);
+        startpos = transform.position;
+        transform.position = firePoint.position;
         Launch();
     }
 
@@ -27,32 +37,50 @@ public class BlueBullet : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
-        transform.LookAt(BlueTankBullet.instace.enemy.transform.position);
+        transform.LookAt(blueTank.transform.position);
+
+        Debug.DrawLine(firePoint.transform.position, blueTank2, Color.blue);
     }
     void Launch()
     {
-        float targetX = (redTank.position.x - transform.position.x);
-        float targetZ = (redTank.position.z - transform.position.z);
-        float targetY = (redTank.position.y - transform.position.y);
+        float targetX = (blueTank2.x - transform.position.x);
+        float targetZ = (blueTank2.z - transform.position.z);
+        float targetY = (blueTank2.y - transform.position.y);
 
-        float distance = Vector3.Distance(redTank.position, transform.position);
+        float distance = Vector3.Distance(blueTank2, transform.position);
 
-        float xAng = Mathf.Sqrt(Mathf.Pow(targetX, 2) + Mathf.Pow(targetZ, 2));
+        //float xAng = Mathf.Sqrt(Mathf.Pow(targetX, 2) + Mathf.Pow(targetZ, 2));
 
-        float square = (Mathf.Pow(asda.instance.v, 4)) - (Physics.gravity.y * (Physics.gravity.y * (Mathf.Pow(targetX, 2) + 2 * targetY * Mathf.Pow(asda.instance.v, 2))));
+        float square = Mathf.Abs(Mathf.Pow(velocity, 4) - (Physics.gravity.y * (Physics.gravity.y * (Mathf.Pow(Mathf.Abs(targetZ), 2)) + (2 * Mathf.Abs(targetY) * Mathf.Pow(velocity, 2)))));
 
-        if (square < 0)
-        {
-            Debug.Log("Not enough range");
-            square = -square;
-        }
+        angle = (Mathf.Pow(velocity, 2) + Mathf.Sqrt(square)) / (Physics.gravity.y * Mathf.Abs(targetZ));
 
-        angle = ((Mathf.Pow(asda.instance.v, 2)) + (Mathf.Sqrt(square) / (Physics.gravity.y * targetX)));
+        Debug.Log(angle);
 
-       // rb.velocity =( angle * asda.instance.v)*transform.
+        float finalAngle = (Mathf.Atan(Mathf.Abs(angle)));
+        Debug.Log(finalAngle * Mathf.Rad2Deg);
 
+        Vector3 projectileXZPos = new Vector3(firePoint.transform.position.x, 0.0f, firePoint.transform.position.z);
+        Vector3 targetXZPos = new Vector3(blueTank2.x, 0.0f, blueTank2.z);
 
-       //Debug.Log(angle);
-        float finalAngle = (angle * Mathf.Rad2Deg);
+        float R = Vector3.Distance(projectileXZPos, targetXZPos);
+        float G = Physics.gravity.y;
+        float tanAlpha = Mathf.Tan(Mathf.Abs(finalAngle) * Mathf.Rad2Deg);
+        float H = blueTank2.y - firePoint.position.y;
+
+        // calculate the local space components of the velocity 
+        // required to land the projectile on the target object 
+        float Vz = Mathf.Sqrt(G * R * R / (2.0f * (H - R * tanAlpha)));
+        float Vy = tanAlpha * Vz;
+
+        localVelocity = new Vector3(0f, Vy, Vz);
+
+        globalVelocity = transform.TransformDirection(localVelocity);
+
+        rb.velocity = globalVelocity;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if()
     }
 }
